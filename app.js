f47a1cdfe65c96f7e1ff86d355c66b703df546cd
Niklas-Seppala <niklas.t.seppala@gmail.com>
2021-12-10 23:52:36 +0200

    cors for dev

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
 'use strict';
-const express = require('express');
-const app = express();
-
 require('dotenv').config();
-
+const express = require('express');
 const cors = require('cors');
-
-const fs = require('fs');
-
-const swaggerUI = require("swagger-ui-express");
-const swagger_docs = require('./swagger_output.json');
-
-const sslkey = fs.readFileSync('ssl-key.pem');
-const sslcert = fs.readFileSync('ssl-cert.pem');
-
-const options = {
-  key: sslkey,
-  cert: sslcert,
-};
-
-// sync the database schema with the defined models
-
-const sequelize = require('./database/sequelize_init.js');
-const db = require('./database/db');
-
 const userRouter = require('./routes/userRouter');
 
 const recipeRouter = require('./routes/recipeRouter');
@@ -34,39 +12,60 @@ const ingredientRouter = require('./routes/ingredientRouter');
 const passport = require('./utils/pass.js');
 const authRoute = require('./routes/authRoute.js');
 
-console.log(
-  `Starting local port: ${process.env.HTTP_PORT} https port: ${process.env.HTTPS_PORT} port: ${process.env.PORT}`
-);
+const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+// const fs = require('fs');
+
+// const swaggerUI = require("swagger-ui-express");
+// const swagger_docs = require('./swagger_output.json');
+
+// const sslkey = fs.readFileSync('ssl-key.pem');
+// const sslcert = fs.readFileSync('ssl-cert.pem');
+
+// const options = {
+//   key: sslkey,
+//   cert: sslcert,
+// };
+
+// sync the database schema with the defined models
+
+// const sequelize = require('./database/sequelize_init.js');
+// const db = require('./database/db');
+
+
+
+// console.log(
+//   `Starting local port: ${process.env.HTTP_PORT} https port: ${process.env.HTTPS_PORT} port: ${process.env.PORT}`
+// );
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.enable('trust proxy');
+// app.enable('trust proxy');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if (process.env.NODE_ENV === 'production') {
-  console.log('Node env prod');
-  require('./utils/production')(app, process.env.PORT, process.env.HTTP_PORT, options);
-  app.listen(process.env.HTTP_PORT);
-} else {
-  console.log('Node env localhost');
-  require('./utils/localhost')(
-    app,
-    process.env.HTTPS_PORT,
-    process.env.HTTP_PORT,
-    options
-  );
-}
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('Node env prod');
+//   require('./utils/production')(app, process.env.PORT, process.env.HTTP_PORT, options);
+
+//   app.listen(process.env.HTTP_PORT);
+// } else {
+//   console.log('Node env localhost');
+//   require('./utils/localhost')(
+//     app,
+//     process.env.HTTPS_PORT,
+//     process.env.HTTP_PORT,
+//     options
+//   );
+// }
 
 // API docs
-const swaggerOptions = {
-  customCss: ".try-out__btn {display: none !important;}",
-  customSiteTitle: "Culinari REST API"
-}
-
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swagger_docs, swaggerOptions));
-
+// const swaggerOptions = {
+//   customCss: ".try-out__btn {display: none !important;}",
+//   customSiteTitle: "Culinari REST API"
+// }
+// app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swagger_docs, swaggerOptions));
 
 app.use('/auth', authRoute);
 app.use('/recipe', recipeRouter);
@@ -81,3 +80,5 @@ app.use((err, req, res, next) => {
   const status = err.status || 500;
   res.status(status).json({ msg: err.msg || 'internal error' });
 });
+
+app.listen(process.env.HTTP_PORT)
